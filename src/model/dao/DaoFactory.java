a2c2e95a93f59b222c5558cf9f92cb1dd2fc18c0
Alex Bezerra <alexgbezerra@hotmail.com>
2023-02-24 18:16:39 -0300

    Implementação e teste do DepartmentDao

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,13 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.JDBC.DepartmentDaoJDBC;
 import model.dao.JDBC.SellerDaoJDBC;
 
+
 public class DaoFactory {
-	
+
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
-		
 	}
-
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
